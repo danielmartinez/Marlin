30ebebd2c8ef1c8c2f6fffbb177629fe1d8e45fe
Daniel Martinez <dmartinez@src.gnome.org>
2015-01-14 09:27:58 +0100

    Change thermistor setup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -142,8 +142,8 @@
 // 147 is Pt100 with 4k7 pullup
 // 110 is Pt100 with 1k pullup (non standard)
 
-#define TEMP_SENSOR_0 1
-#define TEMP_SENSOR_1 1
+#define TEMP_SENSOR_0 11
+#define TEMP_SENSOR_1 11
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_BED 1
 
