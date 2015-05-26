8f8d34ce114bdbf52792f091bfc83fbbe408bd9b
Daniel Martinez <dmartinez@src.gnome.org>
2015-05-26 13:52:42 +0200

    Update Z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -497,7 +497,7 @@ const bool Z_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the logic
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER 61     // Probe on: -left  +right
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 0      // Probe on: -front +behind
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.59  // -below (always!)
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.40  // -below (always!)
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
